117edb2bd8686249ce9d8769fa76dfadadc9fc9d
Toasterbirb <git.53jvg@aleeas.com>
2021-11-02 00:33:20 +0200

    Removed old index.html and centered the site

--- generate.sh
+++ generate.sh
@@ -7,7 +7,15 @@ function _insertHead()
 {
 	file=$1
 	echo "Inserting <head> $file"
-	sed -i '1s|^|<head>\n\t<link rel = "stylesheet" type="text/css" href="style.css">\n</head>\n|' $file
+	sed -i '1s|^|<head>\n<link rel = "stylesheet" type="text/css" href="style.css">\n</head>\n|' $file
+}
+
+function _insertHtml()
+{
+	file=$1
+	echo "Inserting <html> $file"
+	sed -i '1s|^|<html>\n|' $file
+	echo "</html>" >> $file
 }
 
 # Move stylesheet to the build directory
@@ -23,6 +31,7 @@ do
 			# Generate html
 			md2html $i > $htmlpath
 			_insertHead "$htmlpath"
+			_insertHtml "$htmlpath"
 
 			# Replace markdown links to html links
 			sed -i s/\.md/.html/g $htmlpath
