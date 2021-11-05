e8b9837e6e60856391d828f1b9a1fd82aa450d27
Toasterbirb <git.53jvg@aleeas.com>
2021-11-05 18:56:00 +0200

    Some actual content already
    The generation script has seen some updates and also some sort of home
    page is now there. The contact-me page might still need some work
    though. More pages soon to come

--- generate.sh
+++ generate.sh
@@ -3,11 +3,47 @@ contentPath=./content
 sitePath=./docs
 stylesheet=$contentPath/style.css
 
-function _insertHead()
+function _setupSourceMeta()
+{
+	for i in $contentPath/*
+	do
+		case $i in
+			*.md)
+				file="$i"
+
+				grep -q ';navlink' $file && navtitle=$(awk '/^;navlink/' $file | cut -d'=' -f2) && navlinks="<li><a href='$(basename "$file")'>$navtitle</a></li>${navlinks}"
+		;;
+		esac
+	done
+}
+
+function _cleanSourceMeta()
 {
 	file=$1
-	echo "Inserting <head> $file"
-	sed -i '1s|^|<head>\n<link rel = "stylesheet" type="text/css" href="style.css">\n</head>\n|' $file
+	awk '/<p>;/' $file | sed -i 's/<p>//g; s/<\/p>//g' $file
+}
+
+function _insertHeadAndBody()
+{
+	file=$1
+	echo "Inserting <head> and <body> $file"
+
+	title="$(awk '/^;title/' $file | cut -d'=' -f2)"
+	echo "Title: $title"
+
+	sed -i "1s|^|<head>\n<link rel = 'stylesheet' type='text/css' href='style.css'>\n<title>\n$title\n</title>\n</head>\n<body>\n|" $file
+	echo "</body>" >> $file
+}
+
+function _insertNavigation()
+{
+	file=$1
+	echo "Inserting navigation links $file"
+	sed -i "/^<body>/a <nav> \
+		<ul> \
+			$navlinks \
+		</ul>\
+	</nav>" $file
 }
 
 function _insertHtml()
@@ -26,16 +62,29 @@ function _inserMeta()
 	
 	file=$1
 	echo "Inserting metadata $file"
-	sed -i '/^<head>/a \
-<meta property="og:type" content="website"> \
-<meta property="og:site_name" content="toasterbirb.com"> \
-<meta property="og:title" content="toasterbirb.com"> \
-<meta property="og:description" content="Toasterbirbs internet corner"> \
-<meta property="og:image" content="https://duckduckgo.com/i/3be2cf68.png">' $file
+	sed -i "/^<head>/a \
+<link rel=\"shortcut icon\" type=\"image/jpg\" href=\"./pics/jumala.png\"> \
+<meta property=\"og:type\" content=\"website\"> \
+<meta property=\"og:site_name\" content=\"toasterbirb.com\"> \
+<meta property=\"og:title\" content=\"toasterbirb.com\"> \
+<meta property=\"og:description\" content=\"Toasterbirb's internet corner\"> \
+<meta property=\"og:image\" content=\"./pics/jumala.png\">" $file
 }
 
-# Move stylesheet to the build directory
+function _removeSourceMeta()
+{
+	# This should be executed last. It will remove all informational metadata from the top of the file
+	echo "Removing source file metadata"
+	file=$1
+	sed -i '/^<p>;/d; /^;/d' $file
+}
+
+# Move stylesheet to the build directory and clean old html files
 rsync -a $stylesheet $sitePath/style.css
+rsync -a --delete-after $contentPath/pics $sitePath/
+rm $sitePath/*.html
+
+_setupSourceMeta
 
 for i in $contentPath/*
 do
@@ -46,12 +95,17 @@ do
 
 			# Generate html
 			md2html $i > $htmlpath
-			_insertHead "$htmlpath"
+			
+			_cleanSourceMeta "$htmlpath"
+			_insertHeadAndBody "$htmlpath"
 			_insertHtml "$htmlpath"
 			_inserMeta "$htmlpath"
+			_insertNavigation "$htmlpath"
+			_removeSourceMeta "$htmlpath"
 
 			# Replace markdown links to html links
 			sed -i s/\.md/.html/g $htmlpath
+			printf "\n"
 			;;
 	esac
 done
